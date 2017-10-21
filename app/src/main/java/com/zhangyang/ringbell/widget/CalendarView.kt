4af9086d72e0d912d207e67c29897ffcdc2c97e7
ZhangYang <942398220@qq.com>
2017-10-21 21:04:43 +0800

    fragment的切换

--- app/src/main/java/com/zhangyang/ringbell/widget/CalendarView.kt
+++ app/src/main/java/com/zhangyang/ringbell/widget/CalendarView.kt
@@ -147,7 +147,6 @@ class CalendarView(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) :
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         when (event?.action){
-
             MotionEvent.ACTION_DOWN -> {
                 downX = event.x
                 downY = event.y
